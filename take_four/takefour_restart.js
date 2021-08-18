386e6a422de99afb68a079fa6d6a08d70d9ef404
calebrauxa <cmoore@rauxa.com>
2021-08-18 09:39:17 -0700

    restart work

--- take_four/takefour_restart.js
+++ take_four/takefour_restart.js
@@ -1,52 +1,57 @@
-// PLAY / PAUSE BUTTON
+const audioElement1 = document.querySelector('audio');
+
+// web audio api setup 
+const audioContext = new AudioContext();
+const pipedAudio1 = audioContext.createMediaElementSource(audioElement1);
+pipedAudio1.connect(audioContext.destination);
+
+// dom elements 
 const playPauseButton = document.querySelector('.playpause');
 const stopButton = document.querySelector('.stop');
 const progressBar = document.querySelector('.progressbar');
 let positionDiv = document.querySelector('.p1'); // grab the position line div
 
-
-const audioOne = document.querySelector('audio');
-
 let counter = 0;
 
-let audioLength = audioOne.duration; // audio loop lengths in seconds
+let audioLength = audioElement1.duration; // audio loop lengths in seconds
 let pxWidth = progressBar.offsetWidth; // width of player container divs   
 let offsetAmount = Math.random() * audioLength;  // random starting time for each audio
 let percentageComplete = (offsetAmount / audioLength); // percentage of audio loop completed AT START
 let pixelsCompleteOfDiv = Math.floor(percentageComplete * pxWidth); // pixel position of position line div AT START
 
-playPauseButton.addEventListener('click', (e)=>{
-  counter++; // increment counter
-  playIt();
-  e.target.classList.toggle('pause'); // toggle pause class
-  if (!e.target.classList.contains('pause')) {
-    pauseIt();
+
+// PLAY/PAUSE functionality
+playPauseButton.addEventListener('click', function() {
+
+  // check if context is in suspended state (autoplay policy)
+  if (audioContext.state === 'suspended') {
+      audioContext.resume();
   }
-})
+
+  // play or pause track depending on state
+  if (this.dataset.playing === 'false') {
+      audioElement1.play();
+      this.dataset.playing = 'true';
+  } else if (this.dataset.playing === 'true') {
+      audioElement1.pause();
+      this.dataset.playing = 'false';
+  }
+
+}, false);
 
 
 
 
 // STOP BUTTON 
 
-stopButton.addEventListener('click', stopIt);
 audioOne.loop = true;
 
 
-// PLAY MUSIC
-function playIt() {
-  audioOne.currentTime = offsetAmount; // set audio current time to offset amount
-  audioOne.play();
-  
-}
 
-// PAUSE MUSIC
-function pauseIt() {
-  audioOne.pause();
-}
 
 // STOP MUSIC
-function stopIt() {
-  audioOne.pause();
-  audioOne.currentTime = 0;
-}
+// stopButton.addEventListener('click', stopIt);
+// function stopIt() {
+//   audioOne.pause();
+//   audioOne.currentTime = 0;
+// }
